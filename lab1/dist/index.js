e30d6b429d8706452084b43aa501d335014e02fc
MateuszK0s <kosmateusz99@gmail.com>
2021-03-21 19:33:26 +0100

    Node ignore

--- lab1/dist/index.js
+++ lab1/dist/index.js
@@ -1,9 +1,44 @@
-"use strict";
 var Main = /** @class */ (function () {
     function Main() {
-        console.log("nomdsadsac ja piepsze");
+        this.startApp();
     }
-    Main.prototype.halo = function () {
+    Main.prototype.startApp = function () {
+        this.getInputs();
+        this.watchInputValues();
+    };
+    Main.prototype.getInputs = function () {
+        this.l1Input = document.querySelector('#l1');
+        this.l2Input = document.querySelector('#l2');
+        this.l3Input = document.querySelector('#l3');
+        this.l4Input = document.querySelector('#l4');
+        this.sumInput = document.querySelector('#sum');
+        this.avgInput = document.querySelector('#avg');
+        this.minInput = document.querySelector('#min');
+        this.maxInput = document.querySelector('#max');
+    };
+    Main.prototype.watchInputValues = function () {
+        var _this = this;
+        this.l1Input.addEventListener('input', function () { return _this.computeData(); });
+        this.l2Input.addEventListener('input', function () { return _this.computeData(); });
+        this.l3Input.addEventListener('input', function () { return _this.computeData(); });
+        this.l4Input.addEventListener('input', function () { return _this.computeData(); });
+    };
+    Main.prototype.computeData = function () {
+        var l1 = +this.l1Input.value;
+        var l2 = +this.l1Input.value;
+        var l3 = +this.l1Input.value;
+        var l4 = +this.l1Input.value;
+        var sum = l1 + l2 + l3 + l4;
+        var avg = sum / 4;
+        var min = Math.min(l1, l2, l3, l4);
+        var max = Math.max(l1, l2, l3, l4);
+        this.showStats(sum, avg, min, max);
+    };
+    Main.prototype.showStats = function (sum, avg, min, max) {
+        this.sumInput.value = sum.toString();
+        this.avgInput.value = avg.toString();
+        this.minInput.value = min.toString();
+        this.maxInput.value = max.toString();
     };
     return Main;
 }());
--- lab1/src/index.ts
+++ lab1/src/index.ts
@@ -1,11 +1,55 @@
 class Main{
+    l1Input: HTMLInputElement;
+    l2Input: HTMLInputElement;
+    l3Input: HTMLInputElement;
+    l4Input: HTMLInputElement;
+    sumInput: HTMLInputElement;
+    avgInput: HTMLInputElement;
+    minInput: HTMLInputElement;
+    maxInput: HTMLInputElement;
     constructor(){
-        console.log("nomdsadsac ja piepsze")
+        this.startApp();
+    }
+    startApp(){
+        this.getInputs();
+        this.watchInputValues();
+    }
+    getInputs(){
+        this.l1Input = document.querySelector('#l1');
+        this.l2Input = document.querySelector('#l2');
+        this.l3Input = document.querySelector('#l3');
+        this.l4Input = document.querySelector('#l4');
+        this.sumInput = document.querySelector('#sum');
+        this.avgInput = document.querySelector('#avg');
+        this.minInput = document.querySelector('#min');
+        this.maxInput = document.querySelector('#max');
+    }
+    watchInputValues(){
+        this.l1Input.addEventListener('input', () => this.computeData());
+        this.l2Input.addEventListener('input', () => this.computeData());
+        this.l3Input.addEventListener('input', () => this.computeData());
+        this.l4Input.addEventListener('input', () => this.computeData());
+    }
+    computeData(){
+        const l1 = +this.l1Input.value;
+        const l2 = +this.l1Input.value;
+        const l3 = +this.l1Input.value;
+        const l4 = +this.l1Input.value;
+        const sum = l1+l2+l3+l4;
+        const avg = sum/4;
+        const min = Math.min(l1,l2,l3,l4);
+        const max = Math.max(l1,l2,l3,l4);
+
+        this.showStats(sum,avg,min,max);
     }
 
-    halo(){
-        
+    showStats(sum: number,avg: number,min: number,max: number){
+        this.sumInput.value = sum.toString();
+        this.avgInput.value = avg.toString();
+        this.minInput.value = min.toString();
+        this.maxInput.value = max.toString();
     }
+
 }
 
 new Main();
